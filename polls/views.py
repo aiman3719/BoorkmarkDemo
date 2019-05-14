bcbb6469d968cdcb0cd7188e9d70ec03669c3308
aiman3719 <aiman3719@gmail.com>
2019-05-15 00:12:42 +0900

    + polls / detail fbt 추가 및 404에러 고전적처리

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import loader
 
@@ -16,7 +16,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects..get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    
+    return render(request, 'polls/detail.html', context={'question':question})
 
 def result(request, question_id):
     response = "You're looking at the result of question %s."

73c8d3c9f043f4672c5753348889303f7cd9af1a
aiman3719 <aiman3719@gmail.com>
2019-05-15 00:04:31 +0900

    + polls/index뷰에 tempate loader적용

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 from polls.models import Question
 
@@ -7,10 +8,13 @@ from polls.models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
-    output = ', '.join([q.question_text for q in latest_question_list])
+    tempate = loader.get_template('polls//index.html')
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
 
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return HttpResponse(output)
+    return HttpResponse(tempate.render(context, request))
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)

24b8f8c7b2b2a99d5147ec870c908aae31928d7e
aiman3719 <aiman3719@gmail.com>
2019-05-19 00:07:01 +0900

    + timezone import 오류    from django.utils import timezone

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,11 @@
 import datetime
-from time import timezone
 
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -12,7 +14,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):

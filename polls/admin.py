81c335785c06166b86fb0da07efab03102982920
aiman3719 <aiman3719@gmail.com>
2019-05-21 19:25:09 +0900

    + Add QuestionAdmin  and ChoiceInline

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date infomation', {'fields':['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice, ChoiceInline)   # Inline타입은 별도로 Question처럼 임포터할 수 없다.

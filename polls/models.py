82c4745f7bbfd5f88241c52da3e81969a6ed0d81
aiman3719 <aiman3719@gmail.com>
2019-05-21 19:37:11 +0900

    + Add list_display attribute to QuestionAdmin for chage better more display format + add was_published_recently.xxxxx  for method field  to be be better display format

--- polls/models.py
+++ polls/models.py
@@ -18,6 +18,11 @@ class Question(models.Model):
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
+    was_published_recently.admin_order_field = 'pub_date'     # 원칙적으로 임의의메서드에 의한 값은 정렬이 불가한데, 다른으로 정렬.
+    was_published_recently.boolean = True    # 값이 boolean값 형태인지 설정합니다.
+    was_published_recently.shot_description = 'Published recently'   # 항목의 헤더이름을 설정합니다.
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)

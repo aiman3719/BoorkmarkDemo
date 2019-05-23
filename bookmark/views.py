d4517b168ee1316c90ca20118e45a57e1cee3e12
aiman3719 <aiman3719@gmail.com>
2019-05-23 20:31:48 +0900

    + add: 상세뷰기능 추가

--- bookmark/views.py
+++ bookmark/views.py
@@ -2,18 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 from bookmark.models import Bookmark
 
 
 class BookmarkListView(ListView):
+    # template_name = 'bookmark/bookmark_list.html'
     model = Bookmark
 
 
 
 class BookmarkCreateView(CreateView):
+    # template_name = 'bookmark/bookmark_create.html'
     model = Bookmark
     fields = ['site_name', 'url']
-    success_url = reverse_lazy('list')
+    success_url = reverse_lazy('bookmark:list')
     template_name_suffix = '_create'
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = '_update'
+
+

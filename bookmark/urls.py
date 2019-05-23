da790a81722f90f59e672b8e78b072b9e110240f
aiman3719 <aiman3719@gmail.com>
2019-05-23 19:34:08 +0900

    + Add BookmarkCreateView,, url.add, create template, list tempate

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from bookmark.views import BookmarkListView
+from bookmark.views import BookmarkListView, BookmarkCreateView
 from . import views
 
-app_name = 'bookmark'
+# app_name = 'bookmark'  # 이게 붙으니까 뷰에서 reverse_lazy()가 되지않는다.
 
 urlpatterns = [
     # ex: /polls/
     # path('', views.IndexView.as_view(), name='index'),
-    path('', BookmarkListView.as_view(), name='list'),
+    path(r'', BookmarkListView.as_view(), name='list'),
+    path(r'add/', BookmarkCreateView.as_view(), name='add'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from bookmark.models import Bookmark
 
 
 class BookmarkListView(ListView):
     model = Bookmark
+
+
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'

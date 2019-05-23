d6282ccdb97068e14b8afa3a60867c7e5958804b
aiman3719 <aiman3719@gmail.com>
2019-05-23 23:39:20 +0900

    + add url for update and delete

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailView
+from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailView, BookmarkUpdateView, \
+    BookmarkDeleteView
 from . import views
 
 app_name = 'bookmark'  #  app_name 을 사용하면 뷰에서 reverse_lazy(app_name:view_name)형식으로 호출해야한다.
@@ -11,4 +12,6 @@ urlpatterns = [
     path(r'', BookmarkListView.as_view(), name='list'),
     path(r'add/', BookmarkCreateView.as_view(), name='add'),
     path(r'detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
+    path(r'update/<int:pk>/', BookmarkUpdateView.as_view(), name='update'),
+    path(r'delete/<int:pk>/', BookmarkDeleteView.as_view(), name='delete'),
 ]

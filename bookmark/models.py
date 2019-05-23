adc17fc296c84c39ce394f575905bf13a6064969
aiman3719 <aiman3719@gmail.com>
2019-05-23 23:38:41 +0900

    + Add detail function and get_absolute_url() to redirect path

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('site URL')
@@ -8,4 +11,7 @@ class Bookmark(models.Model):
     def __str__(self):
         # 객체를 출력할 떄 나타나는 값
         return "명칭: " + self.site_name + ",    주소 : " + self.url
+
+    def get_absolute_url(self):
+        return reverse('bookmark:detail', args=[str(self.id)])
 

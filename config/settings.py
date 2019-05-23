964e7e581fc8828b89ea681d09e02b07adc2a4c9
aiman3719 <aiman3719@gmail.com>
2019-05-23 20:33:08 +0900

    + Change : in INSTALLED_APPS : from polls.apps.PollsConfig to just polls

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'bookmark',
-    'polls.apps.PollsConfig',
+    'polls',
+    # 'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

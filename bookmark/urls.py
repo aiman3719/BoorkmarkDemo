05468f4edf0d3d1c6552eb6c83dabf63abccdb7f
aiman3719 <aiman3719@gmail.com>
2019-05-23 20:30:55 +0900

    + update : app_name 을 확용한 namespace적용

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from bookmark.views import BookmarkListView, BookmarkCreateView
+from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailView
 from . import views
 
-# app_name = 'bookmark'  # 이게 붙으니까 뷰에서 reverse_lazy()가 되지않는다.
+app_name = 'bookmark'  #  app_name 을 사용하면 뷰에서 reverse_lazy(app_name:view_name)형식으로 호출해야한다.
 
 urlpatterns = [
     # ex: /polls/
     # path('', views.IndexView.as_view(), name='index'),
     path(r'', BookmarkListView.as_view(), name='list'),
     path(r'add/', BookmarkCreateView.as_view(), name='add'),
+    path(r'detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
 ]
